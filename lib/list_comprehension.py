3c9da91814a81896124d80e5d1b7e7df09a12d49
yiranshu <shuyiran1425@hotmail.com>
2024-05-01 10:56:37 -0400

    exclamation

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if n%2==0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentence = [f"{n}"+"!" for n in sentence_list]
+    return exclamation_sentence
